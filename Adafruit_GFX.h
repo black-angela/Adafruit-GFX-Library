4885bf817b5774816e91d09dfa0323d654df8087
blackangelagaga <1119672387@qq.com>
2021-06-03 15:56:16 +0800

    test

--- Adafruit_GFX.h
+++ Adafruit_GFX.h
@@ -69,6 +69,8 @@ class Adafruit_GFX : public Print {
       int16_t w, int16_t h, uint16_t color),
     drawBitmap(int16_t x, int16_t y, const uint8_t bitmap[],
       int16_t w, int16_t h, uint16_t color, uint16_t bg),
+    drawhouse(int16_t x, int16_t y,
+          int16_t w, int16_t h, uint16_t color),
     drawBitmap(int16_t x, int16_t y, uint8_t *bitmap,
       int16_t w, int16_t h, uint16_t color),
     drawBitmap(int16_t x, int16_t y, uint8_t *bitmap,
